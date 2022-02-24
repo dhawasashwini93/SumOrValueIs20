c96d804fa64ba739120d68c1e97497d96fe88e84
ashwini <ashwini.d@healthmeterservices.in>
2022-02-24 17:02:26 +0530

    Add Files

---
+++ SumOrValueIs20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SumOrValueIs20
+//
+//  Created by Demo on 24/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
